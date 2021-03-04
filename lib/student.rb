e316678c273496bb407549d1ec6deb28d50a277d
Jabair <jabairk@gmail.com>
2021-03-03 19:41:50 -0500

    commit tree 9e89e0af7bb6bb7572bb227076c8f29a5d3ba2ba

--- lib/student.rb
+++ lib/student.rb
@@ -2,5 +2,21 @@ class Student
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
+  attr_accessor :name, :grade, :id 
+
+  def initialize(name, grade, id=nil)
+    @name = name
+    @grade = grade 
+    @id = id 
   
+  end
+
+  def self.create_table
+    sql = <<-SQL 
+      "CREATE TABLE IF NOT EXISTS songs (
+        id INTEGER PRIMARY KEY,
+        name TEXT
+        grade TEXT
+      );"
+  end
 end
